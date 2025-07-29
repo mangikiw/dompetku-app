fb93174ad556ed04468b96fcfc2d5081ab92c663
mangikiw <mangikiwwdigital@gmail.com>
2025-07-29 09:00:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('Service Worker Activated');
-});
